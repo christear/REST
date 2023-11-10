624ef2a9cb71b3684e6a1d1d34a25fef5f907b45
christear <zb.picb@gmail.com>
2023-11-10 16:22:55 +0300

    fixed bugs of search4motif

--- search4motif.py
+++ search4motif.py
@@ -15,42 +15,52 @@ def readMotif(motif_file):
         for _line in m:
             _line = _line.rstrip()
             motifs.append(_line)
+    return motifs
             
 # search for motifs in the provided fasta file 
 def search4motif (input_fa,motif_file,start,end,out_tab):
     motifs = readMotif(motif_file)
+    print(motifs)
     sequences = {}
     _start = int(start)
     _end = int(end)
     with open(input_fa,'r') as f:
-        _id = ''
+        #_id = ''
+        _id = 0
         _seq = ''
         _n = 0
         for _line in f:
             _line = _line.rstrip()
             if '>' in _line:
-                _id = _line[1:]
-                _n += 1
-                _seq = ''
+                #_id = _line[1:]
+                #_n += 1
+                if _id != 0:
+                    sequences[str(_id)] = _seq
+                    _seq = ''
+                _id += 1
+                    #_seq = ''
             else:
                 _subs = _line.upper()
                 _seq = _seq + _subs
-                sequence[_id] = _seq
+        sequences[str(_id)] = _seq
     with open(out_tab,'w') as w:
-        outs = []
         _sep = '\t'
         for _id in sequences:
+            outs = []
             outs.append(_id)
             _seq = sequences[_id]
+            #print(_id,_seq)
             _subs = _seq[_start : _end]
-            positions = []
+            #positions = []
             for _m in motifs:
                 _p = -1
                 if _m in _subs:
                     _p = _subs.index(_m)
-                positions.append(_p)
-        _out_line = _sep.join(str(e) for e in outs) + '\n'
-        w.write(_out_line)
+                    #print(_m,_p)
+                #positions.append(_p)
+                outs.append(_p + 1)
+            _out_line = _sep.join(str(e) for e in outs) + '\n'
+            w.write(_out_line)
         
 #"""
 if len(sys.argv) < 3:
